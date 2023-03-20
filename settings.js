7e4489e7065da7b2dce649e25e38c82134fda66d
Dumidu <124751175+Dumidu880@users.noreply.github.com>
2023-03-20 20:28:53 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,10 +18,15 @@ global.ANTI_BADWORD = 'true' // true of false
 global.AUTO_REACT = 'false' // true of false
 global.INBOX_BLOCK_MSG ='BLOCK !!!'
 global.INBOX_BLOCK = 'off' //inbox block
-global.alivemsg = 'ᴅᴀʀᴋ-ɴᴇʀᴏ ʙᴏᴛ'
-global.alivepic = 'https://i.ibb.co/zFYBPcQ/IMG-20230226-WA0115.jpg '
-global.owner = ['0786802371'] //ur owner number
-global.ownername = "kaveesha" //ur owner name
+global.alivemsg = '```Hey There! Bot Online now. 💃🏻♥️```
+
+
+```බොට් ක්‍රියාත්මකයි🌹💕 ```
+*ᴊᴏɪɴ ᴏᴜʀ ᴏꜰꜰᴄɪᴀʟ ʙᴏᴛ ɢʀᴏᴜᴘ*
+   https://chat.whatsapp.com/CpC7BxCdcQAF1buxieRggi  🫶'
+global.alivepic = 'https://telegra.ph/file/90694a331ee80daf0feca.jpg'
+global.owner = ['0742443114'] //ur owner number
+global.ownername = "Dumidu" //ur owner name
 global.ytname = "YT: Dark" //ur yt chanel name
 global.socialm = "GitHub: Dark" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
@@ -34,7 +39,7 @@ global.premium = ['94786802371'] //ur premium number
 global.botname = 'ᴅᴀʀᴋ-ɴᴇʀᴏ ᴡʜᴀᴛʜᴀᴘᴘ ʙᴏᴛ' //ur bot name
 global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://www.darknero.ga/' //script link
+global.botscript = 'https://www/' //script link
 global.themeemoji = "🦄" //ur theme emoji
 global.packname = "Dark Nero By" //ur sticker watermark packname
 global.author = "Dark Nero Creating By Cyber Yakuza Team" //ur sticker watermark author
